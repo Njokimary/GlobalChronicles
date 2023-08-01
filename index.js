378d9e8357503952b932e6db12d79e9872eb99bb
MARY NJOKI <marynjoki738@gmail.com>
2023-08-01 08:38:36 +0300

    created a function to test my url

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const API_KEY = "55c6498d47dc42e58594ea740046d649"
-const URL ="https://newsapi.org/v2/everything? q=bitcoin&apiKey="
+const url ="https://newsapi.org/v2/everything?q="
+
+async function fetchData(query){
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY }`)
+    const data = await res.json()
+    console.log(data)
+}
+fetchData()
