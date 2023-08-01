8a201ba2af701ed771a5af1be2519dfcce4d71ab
MARY NJOKI <marynjoki738@gmail.com>
2023-08-01 11:35:46 +0300

    created a function to fetch our data

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const API_KEY = "55c6498d47dc42e58594ea740046d649"
 const url ="https://newsapi.org/v2/everything?q="
 
+
 async function fetchData(query){
     const res = await fetch(`${url}${query}&apiKey=${API_KEY }`)
     const data = await res.json()
-    console.log(data)
+    return data
 }
-fetchData()
+fetchData("all").then(data => console.log(data))
 
+//menubtn
 let mobilemenu = document.querySelector(".mobile")
 let menuBtn = document.querySelector(".menuBtn")
-let menuBtnDisplay = false;
+let menuBtnDisplay = true;
+
+menuBtn.addEventListener("click",() => {
+    if (menuBtn){
+        mobilemenu.classList.toggle("hidden")
+
+    }
+})
